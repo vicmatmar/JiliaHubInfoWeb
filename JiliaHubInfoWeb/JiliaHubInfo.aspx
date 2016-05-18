2d47843f14d6e084a38d0f0dfb8b44c059885f5e
vicmatmar <victormartin@hotmail.com>
2016-05-18 15:57:13 -0500

    Just display table

---
+++ JiliaHubInfoWeb/JiliaHubInfo.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="JiliaHubInfo.aspx.cs" Inherits="JiliaHubInfoWeb.JiliaHubInfo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+
+            <asp:GridView ID="GridViewInfo" runat="server" Style="margin-top: 17px" Visible="True">
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ JiliaHubInfoWeb/JiliaHubInfo.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace JiliaHubInfoWeb
+{
+    public partial class JiliaHubInfo : System.Web.UI.Page
+    {
+        static SqlConnectionStringBuilder _db_connect_str;
+        static DataTable _table_data;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _db_connect_str = new SqlConnectionStringBuilder(Properties.Settings.Default.ConnectionString);
+
+            if (!IsPostBack)
+            {
+                _table_data = new DataTable();
+                using (SqlConnection con = new SqlConnection(_db_connect_str.ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("select * from JiliaHubs ORDER BY Timestamp DESC", con);
+                    using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                        adp.Fill(_table_data);
+                }
+
+                GridViewInfo.DataSource = _table_data;
+                GridViewInfo.DataBind();
+            }
+        }
+    }
+}
